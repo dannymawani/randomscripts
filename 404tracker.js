68ac072d51b02c750614c128154341851cd0d8cf
Danny Olsen <30554625+dannymawani@users.noreply.github.com>
2017-08-18 23:07:17 +0200

    Create 404tracker.js

---
+++ 404tracker.js
@@ -0,0 +1,12 @@
+
+function () {
+	var returnValue = false;
+	
+	jQuery('h1').each(function() {
+		if(jQuery(this).text().toLowerCase().match(/(404 side ikke fundet)/gi) != null) {
+			returnValue = true;
+		}
+	});
+	
+	return returnValue;
+}
